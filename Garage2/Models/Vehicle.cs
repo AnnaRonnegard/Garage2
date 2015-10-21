3546223672a10c9b135ff4dbd64342df5825bd78
OlgaKag <Olga.Kagyrina@hotmail.se>
2015-10-21 16:25:16 +0200

    Comments for Vehicle Attributes

--- Garage2/Models/Vehicle.cs
+++ Garage2/Models/Vehicle.cs
@@ -11,23 +11,23 @@ namespace Garage2.Models
     {
         
         public int Id { get; set; }        
-        [Required]
-        [StringLength(15, MinimumLength = 3)]      
-        public string Type { get; set; }  //car, plane, motorcycle
-        [Required]
-        [StringLength(10)]
+        [Required]                              //Text box cannot be empty
+        [StringLength(15, MinimumLength = 3)]   //Maximum Length = 15, minimum Length = 3   
+        public string Type { get; set; }        //car, plane, motorcycle
+        [Required]                              //Text box cannot be empty
+        [StringLength(10)]                      //Maximum Length = 10
         public string RegNumber { get; set; }
-        [Required]
-        [StringLength(10, MinimumLength = 3)]       
+        [Required]                              //Text box cannot be empty
+        [StringLength(10, MinimumLength = 3)]   //Maximum Length = 15, minimum Length = 3    
         public string Brand { get; set; }
-        [Required]
-        [StringLength(6)]      
+        [Required]                              //Text box cannot be empty
+        [StringLength(6)]                       //Maximum Length = 10
         public string VehicleModel { get; set; }
-        [Required]
-        [StringLength(12, MinimumLength = 3)]     
+        [Required]                              //Text box cannot be empty
+        [StringLength(12, MinimumLength = 3)]   //Maximum Length = 12, minimum Length = 3
         public string Colour { get; set; }
-        [Range(2,10)]
-        [Required]
+        [Range(2,10)]                           //Range cannot be less then 1 and more then 10
+        [Required]                              //Text box cannot be empty
         public int NumberofWheels { get; set; }
 
     }
