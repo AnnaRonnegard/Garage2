9bae9da5d504f2bbe0bfc451be9f2f0d3c6265ec
AnnaRonnegard <a.ronnegard@gmail.com>
2015-10-22 09:22:13 +0200

    Enum

---
+++ Garage2/Enum/Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Enum
+{
+    public enum Type
+    {
+        Airplane,
+        Boat,
+        Car,
+        Motorcycle
+    }
+}
